09990999f440e8c2a823a5de96a5ffb487e4aadb
hndand934-design <hndand934@gmail.com>
2026-02-22 10:09:48 +0300

    Update wallet.js

--- shared/wallet.js
+++ shared/wallet.js
@@ -1,1 +1,16 @@
+(() => {
+  const KEY = "mini_wallet_global";
 
+  function read() {
+    const v = localStorage.getItem(KEY);
+    return v ? Number(v) : 1000;
+  }
+  function write(v) {
+    localStorage.setItem(KEY, String(v));
+  }
+  function add(n) {
+    write(read() + n);
+  }
+
+  window.Wallet = { read, write, add };
+})();
